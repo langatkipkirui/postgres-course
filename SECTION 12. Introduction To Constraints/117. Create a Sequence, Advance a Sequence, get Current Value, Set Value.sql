a8fa26c3f3493fd95a129ae0b5c899aa27dc848b
Kevin Langat <kevinlangat419@gmail.com>
2026-07-04 18:56:33 +0300

    did the set a sequence to a particular value, Set a sequence and do not skip over and Control the sequence START value

--- SECTION 12. Introduction To Constraints/117. Create a Sequence, Advance a Sequence, get Current Value, Set Value.sql
+++ SECTION 12. Introduction To Constraints/117. Create a Sequence, Advance a Sequence, get Current Value, Set Value.sql
@@ -13,7 +13,26 @@ CREATE SEQUENCE IF NOT EXISTS test_seq;
 -- 2. Advance a sequence and return new value
 SELECT nextval('test_seq');
 
+-- 3. Return the most current value of the sequence
+-- SELECT currval(name)
+SELECT currval('test_seq')
 
+-- 4. Set a sequence 
+-- SELECT setval(name, value)
+SELECT setval('test_seq', 100)
+
+-- 5. Set a sequence and do not skip over
+SELECT setval('test_seq', 200, false)
+
+-- 6. Control the sequence START value
+/*
+CREATE SEQUENCE IF NOT EXISTS name START WITH value
+CREATE SEQUENCE name START WITH value
+*/
+CREATE SEQUENCE IF NOT EXISTS test_seq2 START WITH 50;
+
+-- 7. Lets use the Sequence
+SELECT nextval('test_seq2')
 
 
 
