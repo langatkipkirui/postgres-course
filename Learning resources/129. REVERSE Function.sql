55517fb2fa5439461bb34a2f282e9d81c5414798
Kevin Langat <kevinlangat419@gmail.com>
2026-07-19 21:56:17 +0300

    did the REVERSE function in plpgsql

---
+++ Learning resources/129. REVERSE Function.sql
@@ -0,0 +1,7 @@
+-- 129. REVERSE Function
+-- PostgreSQL reverse() is used to arrange a string in reverse order
+REVERSE(string)
+
+SELECT REVERSE('Amazing PostgreSQL')
+SELECT REVERSE('LQSergtsoP gnizamA')
+
