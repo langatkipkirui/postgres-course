ad1130383ead8a0c767e4e88ce2f32ea908133b5
Kevin Langat <kevinlangat419@gmail.com>
2026-07-22 15:38:32 +0300

    did the POSITION function

---
+++ SECTION 14. String Functions/134. POSITION function.sql
@@ -0,0 +1,25 @@
+-- POSITION function 
+
+/*
+1. PostgreSQL POSITION() function returns the location of a 
+ substring within the string
+
+2. POSITION(substring in string)
+
+3. returns an integer that represents the location of the substring
+
+4. returns the first instance of the substring in the string
+
+5. searches for the substring case-insensitively
+*/
+SELECT POSITION('Amazing' IN 'Amazing PostgreSQL')
+SELECT POSITION('is' IN 'This is a computer')
+SELECT POSITION('Amazing' IN 'Amazing PostgreSQL')
+
+
+SELECT POSTION ('C' IN 'KlickAnalytics.com')
+
+
+
+
+
