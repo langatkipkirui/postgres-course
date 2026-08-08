34253f247eb07812b2c596056239167aa214cd2a
Kevin Langat <kevinlangat419@gmail.com>
2026-08-08 18:11:52 +0300

    did the: Time of day formats and inputs

---
+++ SECTION 16. Using DATE and TIME Functions/149. Time of day formats and inputs.sql
@@ -0,0 +1,59 @@
+-- 149. Time of day formats and inputs
+
+/*
+HH:MM
+
+02:10 AM
+
+06:10 pm 		(internally stored as 18:10, converting into 24 hrs format)
+
+06:10:10		HH:MM:SS ISO 8601 format
+
+06:10:10.786	HH:MM:SS	i.e 10.783 seconds  - 6 decimals places
+
+
+TimeStamp format
+
+date time		date and time are concatenated together with oprional date time tz
+
+date time		2020-01-02 10:30:23			YYYY-MM-DD HH:MM:SS
+
+
+-- Date/Time input formats
+allballs		time
+now()			date, time, timestamp
+
+today			date, timestamp
+
+tomorrow
+yesterday
+epoch
+infinity
+-infinity
+
+
+
+
+
+
+
+
+*/
+
+select now()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
